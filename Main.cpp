82f5dda79fc92e75abbc44f706abeeb6068220a9
Pipsqueak_Dev <adityaconsultancy2008@gmail.com>
2026-08-28 05:20:13 +0000

    h be

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<vector>
+class Mediaengine{
+    private:{}
+     std::vector<std::string> VideoQueue
+
+    public:
+    void addMedia (const std::string& filepath){}
+    
+};
